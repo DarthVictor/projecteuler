e5d72d34423177c4b04841a8142a6155d7afb198
Follet Victor <follet.victor@gmail.com>
2013-06-26 02:55:55 +0400

    Task 10

---
+++ src/Task10.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17
+ * 
+ * Find the sum of all the primes below two million.
+ */
+public class Task10
+{
+	private static boolean dividedByElemOfArray(long number, List<Long> primes)
+	{
+		for(long prime: primes)
+			if(number % prime == 0 && number != prime)
+				return true;
+		return false;
+	}
+	public static void main(String[] args) {
+		List<Long> primes = new LinkedList<Long>();
+		primes.add(2L);
+		long number = 3;
+		long sum = 2;
+		
+		while (number < 2000000)
+		{
+			if (!dividedByElemOfArray(number, primes))
+			{
+				primes.add(number);
+				sum += number;				
+			}
+			if (number % 20000 == 0)
+				System.out.println(number);
+			number++;
+		}
+		System.out.println(sum);
+	}
+}
